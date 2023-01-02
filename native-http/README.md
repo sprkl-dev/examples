b11712d8c5521caf0021ca2b1ca41ee2283663f8
razcoen <raz@sprkl.dev>
2023-01-02 15:40:42 +0200

    docs(native-http): describe how to run and use sprkl

---
+++ native-http/README.md
@@ -0,0 +1,15 @@
+# How to run?
+
+## Running regularly
+
+```bash
+node index.js
+```
+
+## Running with Sprkl
+> Checkout the `index.js` file, uncomment line #5.
+
+```bash
+sprkl -- node index.js
+```
+
--- native-http/index.js
+++ native-http/index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
   console.log({ msg: 'Handling an http request.', path: req.url })
-  // console.log({ msg: 'Testing Sprkl.'})
+  // console.log({ msg: 'Testing Sprkl.'}) // Uncomment me :), then run `sprkl -- node index.js`
   res.write(JSON.stringify({ Hello: 'World' }))
   res.end()
 })
