ade17aa0037836d6117245216e838080e6d5c8db
razcoen <raz@sprkl.dev>
2023-01-02 15:38:12 +0200

    initial commit: native-http example using nodejs native http server (commonjs)

---
+++ native-http/index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  console.log({ msg: 'Handling an http request.', path: req.url })
+  // console.log({ msg: 'Testing Sprkl.'})
+  res.write(JSON.stringify({ Hello: 'World' }))
+  res.end()
+})
+
+const PORT = process.env.PORT ?? 3000
+
+server.listen(PORT, () => {
+  console.log({ msg: 'Server is listening.', port: PORT })
+})
